3d7caf676a05edcd2292454cbc9401af3e26e607
BaiToey <wisanlaya.baitoey@gmail.com>
2020-03-19 00:16:08 +0700

    Initial commit

---
+++ resources/views/bisection.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <head>
+            <title>Bisection Method</title>
+            
+            <!--Sidebar ไอตรงด้านซ้าย -->
+            <div id="mySidebar" class="sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item px-2">
+                        <button type="button" class="close" onclick="closeNav()" style="width: 64px;">&times;</button>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.blade.php">หน้าแรก</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">ตรวจสอบยอดวัตถุดิบ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">รับ|เบิก|คืนวัตถุดิบ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">ประวัติการรับ|เบิก|คืนวัตถุดิบ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">สั่งซื้อวัตถุดิบประจำเดือน</a>
+                    </li>
+                </ul>
+            </div>
+        </head>
+
+        <?php
+        ?>
+
+        <footer>
+        </footer>
+
+    </body>
+</html>
